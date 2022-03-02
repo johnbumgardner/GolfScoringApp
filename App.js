51d06bb8adf392651e21b12d417b3fbd6a4ee725
John Bumgardner <john.bumgardner@icloud.com>
2022-03-02 00:09:17 -0500

    initial commit for react native app

---
+++ App.js
@@ -0,0 +1,142 @@
+import React, { Fragment, Component } from 'react';
+import { launchImageLibrary } from 'react-native-image-picker';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  Button,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const options = {
+  title: 'Select Avatar',
+  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filepath: {
+        data: '',
+        uri: ''
+      },
+      fileData: '',
+      fileUri: ''
+    }
+  }
+
+  launchImageLibrary = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    launchImageLibrary(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = { uri: response.uri };
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri
+        });
+      }
+    });
+
+  }
+  render() {
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <View style={styles.body}>
+            <Text style={{textAlign:'center',fontSize:40,paddingBottom:10}} >Golf Scorer</Text>
+            <Text style={{textAlign:'center',fontSize:20,paddingBottom:10}} >Pick Cropped Image of Score Card from Gallery</Text>
+            <View style={styles.btnParentSection}>
+
+              <TouchableOpacity onPress={this.launchImageLibrary} style={styles.btnSection}  >
+                <Text style={styles.btnText}>Directly Launch Image Library</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+
+  body: {
+    backgroundColor: Colors.white,
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    height: Dimensions.get('screen').height - 20,
+    width: Dimensions.get('screen').width
+  },
+  ImageSections: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    justifyContent: 'center'
+  },
+  images: {
+    width: 150,
+    height: 150,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginHorizontal: 3
+  },
+  btnParentSection: {
+    alignItems: 'center',
+    marginTop:10
+  },
+  btnSection: {
+    width: 225,
+    height: 50,
+    backgroundColor: '#DCDCDC',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+    marginBottom:10
+  },
+  btnText: {
+    textAlign: 'center',
+    color: 'gray',
+    fontSize: 14,
+    fontWeight:'bold'
+  }
+});
